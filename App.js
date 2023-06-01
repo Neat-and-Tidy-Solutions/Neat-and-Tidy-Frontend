91f8ac83c20e7e328199d452967da90573dd8fb1
oreoluwa27 <orework22@gmail.com>
2023-06-01 14:03:38 +0100

    indev commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Heading from "./component/jsx/Body.jsx/Heading";
 import Header from "./component/jsx/Headers.jsx/Header";
+import Login from "./component/jsx/Body.jsx/Login.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Secondbody from "./component/jsx/Secondbody";
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <Heading />
       {/**<Header />**/}
       <Secondbody />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={Heading} />
+          <Route path="/login" component={Login} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
